dbf629a92c06290e3a25c6620f03e9bbb0f2e3bb
Samisha Khurana <32064975+samishakhurana@users.noreply.github.com>
2023-06-21 05:59:30 +0000

    account and customer class created

---
+++ src/main/java/bank/Customer.java
@@ -0,0 +1,58 @@
+package bank;
+
+public class Customer {
+  private int id;
+  private String name;
+  private String userName;
+  private String password;
+  private int accountId;
+
+  public Customer(int id, String name, String userName, String password, int accountId) {
+    setId(id);
+    setName(name);
+    setUserName(userName);
+    setPassword(password);
+    setAccountId(accountId);
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUserName() {
+    return this.userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public int getAccountId() {
+    return this.accountId;
+  }
+
+  public void setAccountId(int accountId) {
+    this.accountId = accountId;
+  }
+
+}
